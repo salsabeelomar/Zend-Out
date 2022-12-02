8fa53def201a1e5b4ec3bc5a507094560c577cb6
salsabeelomar <omarsalsabeel65@gmail.com>
2022-12-02 22:47:10 +0200

    add Proftle app and put it in User navigation relates #3

--- navigation/User.jsx
+++ navigation/User.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignIn from '../Pages/SignIn';
 import SignUp from '../Pages/SignUp';
+import Profile from '../Pages/Profile';
 
 function UserStack() {
   const Stack = createNativeStackNavigator();
   return (
     <Stack.Navigator initialRouteName="Profile">
       <Stack.Screen name="SignIn" component={SignIn} />
-      <Stack.Screen name="Profile" component={SignIn} />
+      <Stack.Screen name="Profile" component={Profile} />
       <Stack.Screen name="SignUp" component={SignUp} />
     </Stack.Navigator>
   );
