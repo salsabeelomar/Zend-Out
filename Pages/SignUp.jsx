3c3d48ce00e1d97db3078a4f77ad1ecf14ac2e7d
salsabeelomar <omarsalsabeel65@gmail.com>
2022-12-03 21:39:47 +0200

    prepare the textInputs relates #7

--- Pages/SignUp.jsx
+++ Pages/SignUp.jsx
@@ -1,7 +1,45 @@
-import { Text } from 'react-native';
-import React from 'react';
+import { TextInput, ScrollView, KeyboardAvoidingView } from 'react-native';
+import React, { useState } from 'react';
 
 function SignUp() {
-  return <Text> signups</Text>;
+  const [user, setUser] = useState({
+    email: '',
+    fName: '',
+    lName: '',
+    password: '',
+  });
+  const handleSignUp = (text, type) => {
+    setUser(prev => ({ ...prev, [type]: text }));
+  };
+  return (
+    <KeyboardAvoidingView>
+      <ScrollView>
+        <TextInput
+          name="fName"
+          value={user.fName}
+          onChangeText={text => handleSignUp(text, 'fName')}
+          placeholder="First name"
+        />
+        <TextInput
+          name="lName"
+          value={user.lName}
+          onChangeText={text => handleSignUp(text, 'lName')}
+          placeholder="Last name"
+        />
+        <TextInput
+          name="email"
+          value={user.email}
+          onChangeText={text => handleSignUp(text, 'email')}
+          placeholder="Email"
+        />
+        <TextInput
+          name="password"
+          value={user.password}
+          onChangeText={text => handleSignUp(text, 'password')}
+          placeholder="Password"
+        />
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
 }
 export default SignUp;
