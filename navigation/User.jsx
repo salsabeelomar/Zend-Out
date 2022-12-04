3b14e4cc99adc518c80862653287a02e5d6622a5
salsabeelomar <omarsalsabeel65@gmail.com>
2022-12-04 22:18:40 +0200

    create Profile page relates #9

--- navigation/User.jsx
+++ navigation/User.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Lottie from 'lottie-react-native';
+import productLoader from '../assets/productLoader.json';
 import SignIn from '../Pages/SignIn';
 import SignUp from '../Pages/SignUp';
 import Profile from '../Pages/Profile';
 
 function UserStack() {
+  const [user, setUser] = useState();
   const Stack = createNativeStackNavigator();
-  return (
+  useEffect(() => {
+    const getUser = async () => {
+      setUser(JSON.parse(await AsyncStorage.getItem('user')));
+    };
+    getUser();
+  }, []);
+  return user === undefined ? (
+    <Lottie source={productLoader} autoPlay loop />
+  ) : (
     <Stack.Navigator
-      initialRouteName="SignUp"
+      initialRouteName={user ? 'Profile' : 'SignUp'}
       screenOptions={{ headerShown: false }}
     >
       <Stack.Screen name="SignIn" component={SignIn} />
-      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen
+        name="Profile"
+        options={{ headerShown: true }}
+        component={Profile}
+      />
       <Stack.Screen name="SignUp" component={SignUp} />
     </Stack.Navigator>
   );
